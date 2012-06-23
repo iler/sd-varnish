c9b9dda696c65ae3bb69113ef7574c254f2d04dd
Ilari Mäkelä <ilari.makela@mearra.com>
2012-06-23 13:53:14 +0300

    Implemented better actual request, hit, miss, hit ratio and miss ratio counters.

--- Varnishstat.py
+++ Varnishstat.py
@@ -21,21 +21,58 @@ class Varnishstat(object):
             value = stat_node.findtext('value')
             stats[label] = value
 	
-	# Count uptime in days
-	uptime = float(stats['Client uptime'])
+	      # Count uptime in days
+	      uptime = float(stats['Client uptime'])
         stats['Client uptime'] = uptime / 86400
 
-	# Count cache hit and miss ratios
-	requests = float(stats['Client requests received'])
-        hits = float(stats['Cache hits'])
-        misses = float(stats['Cache misses'])
-        stats['Hit ratio'] = float(hits / requests) * 100
-        stats['Miss ratio'] = float(misses / requests) * 100
+	      requests_new = int(stats['Client requests received'])
+        hits_new = int(stats['Cache hits'])
+        misses_new = int(stats['Cache misses'])
+        requests_old = int(0)
+        hits_old = int(0)
+        misses_old = int(0)
+        
+        try:
+          with open('/tmp/sdstats', 'r') as sfile:
+            requests_old = int(sfile.readline())
+            hits_old = int(sfile.readline())
+            misses_old = int(sfile.readline())
+            sfile.close()        
+        except IOError as e:
+          sfile = open('/tmp/sdstats', 'w')
+          req = str(requests_new)
+          hit = str(hits_new)
+          miss = str(misses_new)
+          sfile.write(req + '\n' + hit + '\n' + miss)
+          sfile.close()
+          
+        sfile = open('/tmp/sdstats', 'w')
+        req = str(requests_new)
+        hit = str(hits_new)
+        miss = str(misses_new)
+        sfile.write(req + '\n' + hit + '\n' + miss)
+        sfile.close()
+          
+        requests = float(requests_new - requests_old)
+        hits = float(hits_new - hits_old)
+        misses = float(misses_new - misses_old)
+        if requests != 0.00:
+          stats['Hit ratio'] = float(hits / requests) * 100
+          stats['Miss ratio'] = float(misses / requests) * 100
+          stats['Client requests received'] = int(requests)
+          stats['Cache hits'] = int(hits)
+          stats['Cache misses'] = int(misses)
+        else:
+          stats['Hit ratio'] = 0.00
+          stats['Miss ratio'] = 0.00
+          stats['Client requests received'] = 0
+          stats['Cache hits'] = 0
+          stats['Cache misses'] = 0
         
         # Convert body and header bytes to MB
         body_bytes = float(stats['Total body bytes'])
-	stats['Total body MB'] = float(body_bytes / 1048576)
-	header_bytes = float(stats['Total header bytes'])
-	stats['Total header MB'] = float(header_bytes / 1048576)
+	      stats['Total body MB'] = float(body_bytes / 1048576)
+	      header_bytes = float(stats['Total header bytes'])
+	      stats['Total header MB'] = float(header_bytes / 1048576)
         
         return stats
